471f96290d7a47375764844954ef3b9bc6d97790
Daniel Saldaña <danielneon98@hotmail.com>
2020-06-16 11:27:12 -0500

    First commit
    MVP

---
+++ js/main.js
@@ -0,0 +1,35 @@
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+            $('#image2').attr('src', e.target.result);
+            $('#image2').removeClass("hidden");
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+function merge() {
+    var imgCanvas = document.getElementById('image1');
+    var toMerge = document.getElementById('image2');
+
+    var canvas = document.getElementById('canvas');
+    $('#image2').addClass("hidden");
+    $('#divhidden').addClass("hidden");
+    canvas.classList.remove("hidden");
+
+    var context = canvas.getContext('2d');
+
+    canvas.width = 500;
+    canvas.height = 500;
+
+    context.globalAlpha = 1.0;
+    context.drawImage(toMerge, 0, 0, 500, 500);
+    context.globalAlpha = 1.0;
+    context.drawImage(imgCanvas, 0, 0, 500, 500);
+}
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
