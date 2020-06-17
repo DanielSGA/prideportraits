a7c768eb417d5874fa5e5350d46663bd55bb6d15
Daniel Saldaña <danielneon98@hotmail.com>
2020-06-17 14:47:48 -0500

    Remove logo

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 function readURL(input) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
-
         reader.onload = function(e) {
             $('#image2').attr('src', e.target.result);
             $('#image2').removeClass("hidden");
