2a43766eb38fe5ba8699ad4d77b37c3dbc2c83fe
Daniel Saldaña <danielneon98@hotmail.com>
2020-11-05 13:44:06 -0600

    New version Gradient Tool

--- js/main.js
+++ js/main.js
@@ -1,20 +1,62 @@
+
 function readURL(input) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
         reader.onload = function(e) {
-            $('#image2').attr('src', e.target.result);
-            $('#image2').removeClass("hidden");
+            $('#image3').attr('src', e.target.result);
+            $('#image3').removeClass("hidden");
         };
         reader.readAsDataURL(input.files[0]);
     }
 }
 
+function lgbt() {
+    var lgbtimg = document.getElementById('image2');
+    var panimg = document.getElementById('image1');
+    const bodydiv = document.getElementById('bodydiv');
+    var lgbtbtn = document.getElementById('lgbt');
+    var panbtn = document.getElementById('panaf');
+
+    if(lgbtimg.id == 'image2')
+    {
+        lgbtimg.id = 'image1';
+        panimg.id = 'image2';
+    
+        bodydiv.classList.add("bodylgbt");
+        bodydiv.classList.remove("bodypan");
+
+        lgbtbtn.classList.add("btn-glow");
+        panbtn.classList.remove("btn-glow");
+
+    }
+}
+
+function panafrican() {
+    var lgbtimg = document.getElementById('image1');
+    var panimg = document.getElementById('image2');
+    const bodydiv = document.getElementById('bodydiv');
+    var lgbtbtn = document.getElementById('lgbt');
+    var panbtn = document.getElementById('panaf');
+
+    if(panimg.id == 'image2')
+    {
+        lgbtimg.id = 'image2';
+        panimg.id = 'image1';
+    
+        bodydiv.classList.add("bodypan");
+        bodydiv.classList.remove("bodylgbt");
+
+        panbtn.classList.add("btn-glow");
+        lgbtbtn.classList.remove("btn-glow");
+    }
+}
+
 function merge() {
     var imgCanvas = document.getElementById('image1');
-    var toMerge = document.getElementById('image2');
+    var toMerge = document.getElementById('image3');
 
     var canvas = document.getElementById('canvas');
-    $('#image2').addClass("hidden");
+    $('#image3').addClass("hidden");
     $('#divhidden').addClass("hidden");
     canvas.classList.remove("hidden");
 
