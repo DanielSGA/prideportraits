887c6ca67fc8d2d5c298784b77cb2a7c8fb13b69
Daniel Saldaña <danielneon98@hotmail.com>
2021-03-18 22:45:10 -0600

    Added: Trans Rights Filter

--- js/main.js
+++ js/main.js
@@ -44,8 +44,77 @@ function lgbt() {
             lgbtbtn.classList.add("btn-glow");
             activebtn.classList.remove("btn-glow");
 
-            activebtn.id = 'veterans'
+            activebtn.id = 'veterans';
             lgbtbtn.id = 'activebtn';
+        }
+        if(active.classList.contains('transgr'))
+        {
+            active.id = 'image4';
+            lgbtimg.id = 'active';
+
+            bodydiv.classList.add("bodylgbt");
+            bodydiv.classList.remove("bodytrans");
+
+            lgbtbtn.classList.add("btn-glow");
+            activebtn.classList.remove("btn-glow");
+
+            activebtn.id = 'trans';
+            lgbtbtn.id = 'activebtn';
+        }
+        merge();
+    }
+}
+
+function transgender() {
+    var active = document.getElementById('active');
+    var transimg = document.getElementById('image4');
+    const bodydiv = document.getElementById('bodydiv');
+    var activebtn = document.getElementById('activebtn');
+    var transbtn = document.getElementById('trans');
+
+    if(transimg.id == 'image4')
+    {
+        if(active.classList.contains('lgbtgr'))
+        {
+            active.id = 'image1';
+            transimg.id = 'active';
+        
+            bodydiv.classList.add("bodytrans");
+            bodydiv.classList.remove("bodylgbt");
+
+            transbtn.classList.add("btn-glow");
+            activebtn.classList.remove("btn-glow");
+
+            activebtn.id = 'lgbt';
+            transbtn.id = 'activebtn';
+        }
+        if(active.classList.contains('pangr'))
+        {
+            active.id = 'image2';
+            transimg.id = 'active';
+
+            bodydiv.classList.add("bodytrans");
+            bodydiv.classList.remove("bodypan");
+
+            transbtn.classList.add("btn-glow");
+            activebtn.classList.remove("btn-glow");
+
+            activebtn.id ='panaf';
+            transbtn.id = 'activebtn';
+        }
+        if(active.classList.contains('vetgr'))
+        {
+            active.id = 'image3';
+            transimg.id = 'active';
+        
+            bodydiv.classList.add("bodytrans");
+            bodydiv.classList.remove("bodyvet");
+
+            transbtn.classList.add("btn-glow");
+            activebtn.classList.remove("btn-glow");
+
+            activebtn.id = 'veterans';
+            transbtn.id = 'activebtn';
         }
         merge();
     }
@@ -71,7 +140,7 @@ function panafrican() {
             panbtn.classList.add("btn-glow");
             activebtn.classList.remove("btn-glow");
 
-            activebtn.id = 'lgbt'
+            activebtn.id = 'lgbt';
             panbtn.id = 'activebtn';
         }
         if(active.classList.contains('vetgr'))
@@ -85,7 +154,21 @@ function panafrican() {
             panbtn.classList.add("btn-glow");
             activebtn.classList.remove("btn-glow");
 
-            activebtn.id = 'veterans'
+            activebtn.id = 'veterans';
+            panbtn.id = 'activebtn';
+        }
+        if(active.classList.contains('transgr'))
+        {
+            active.id = 'image4';
+            panimg.id = 'active';
+
+            bodydiv.classList.add("bodypan");
+            bodydiv.classList.remove("bodytrans");
+
+            panbtn.classList.add("btn-glow");
+            activebtn.classList.remove("btn-glow");
+
+            activebtn.id = 'trans';
             panbtn.id = 'activebtn';
         }
         merge();
@@ -127,6 +210,20 @@ function veterans() {
             activebtn.classList.remove("btn-glow");
 
             activebtn.id ='panaf';
+            vetbtn.id = 'activebtn';
+        }
+        if(active.classList.contains('transgr'))
+        {
+            active.id = 'image4';
+            vetimg.id = 'active';
+
+            bodydiv.classList.add("bodyvet");
+            bodydiv.classList.remove("bodytrans");
+
+            vetbtn.classList.add("btn-glow");
+            activebtn.classList.remove("btn-glow");
+
+            activebtn.id = 'trans';
             vetbtn.id = 'activebtn';
         }
         merge();
